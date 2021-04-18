312fbecbb0b992a1bd40be201a347b111c0d94d9
PGAAPWdl <sasha.chakalov@gmail.com>
2021-04-18 23:03:34 +0300

    Last changes

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def get_films():
+    return [
+        {
+            'id' : 1,
+            'title' : 'Forsage 1',
+            'release_day' : '2001'
+        },
+        {
+            'id': 2,
+            'title': 'Forsage 2',
+            'release_day': '2002'
+        }
+    ]
 
 @app.route('/')
 def index():
-    return render_template('hello.html')
+    films = get_films()
+    return render_template('hello.html', films = films)
 
 
 @app.route('/about')
